53c3336d9995a266bcde3cb01417556a70924750
Alexander <116946826+Tr13an@users.noreply.github.com>
2024-02-21 07:30:33 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,17 +3,36 @@ $(document).ready(function() {
         window.location.href = "bookings.html";
     });
 
-    $(".timeButton").click(function() {
-        var selectedTime = $(this).data("time");
-        $("#timeSelected").text("Вы выбрали время: " + selectedTime);
+    var selectedMonth = new Date().getMonth();
+    var selectedYear = new Date().getFullYear();
+    var selectedDay = "";
+    var selectedTime = "";
 
-        $("#datepicker").datepicker({
-            minDate: 0,
-            dateFormat: 'yy-mm-dd',
-            defaultDate: new Date(),
-            onSelect: function(dateText) {
-                alert("Вы выбрали дату: " + dateText + " и время: " + selectedTime);
+    $(".timeButton").click(function() {
+        selectedTime = $(this).data("time");
+        $("#timeSelected").text("Вы выбрали время: " + selectedTime);
+    });
+
+    $("#datepicker").datepicker({
+        minDate: 0,
+        dateFormat: 'yy-mm-dd',
+        defaultDate: new Date(),
+        onSelect: function(dateText) {
+            var selectedDate = new Date(dateText);
+            selectedDay = selectedDate.getDate();
+            var weekDays = new Array('Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб');
+            var dayOfWeek = weekDays[selectedDate.getDay()];
+            
+
+            $("#timeSelected").text("Вы выбрали дату: " + selectedDay + "." + (selectedMonth + 1) + "." + selectedYear + " (" + dayOfWeek + ")");
+            $(".timeButtons").html(""); // Очистка предыдущего выбора времени
+            
+            for (let i = 11; i < 22; i++) { // Генерация кнопок времени (для примера 3 кнопки)
+                var newTimeButton = $("<button>").addClass("timeButton").text((i + 1) * 1 + ":00").data("time", (i + 1) * 3 + ":00");
+                $(".timeButtons").append(newTimeButton);
+                    
             }
-        });
+        }
     });
 });
+
